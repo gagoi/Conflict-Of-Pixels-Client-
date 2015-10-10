0eb6d23683680f3e5ba5a5e24c023d7943a43311
Gagoi <harrygag@gmail.com>
2015-10-11 01:19:59 +0200

    Ajout du logger, et gros changements sur le systeme graphique. Adieu les JPanels, et bonjour le canvas.
    Ajout du systeme de tiles (effet pixelisé).
    Recherche pour la caméra également (voir offset).

---
+++ fr/cop/game/core/helpful/logger/SimpleLog.java
@@ -0,0 +1,12 @@
+package fr.cop.game.core.helpful.logger;
+
+public class SimpleLog {
+	
+	public void logTxt(String tag, String message){
+		System.out.println(tag + " " + message);
+	}
+	
+	public void logErr(String tag, String message){
+		System.err.println(tag + " " + message);
+	}
+}

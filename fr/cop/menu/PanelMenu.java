485ee62d9516112af58a84b1367917fb9ce9ff64
Gagoi <Gagoi@Gagoi-PC>
2015-10-18 16:50:12 +0200

    Ajout d'un bouton twitter. (pour le moment renvoies sur mon profil). Ajout d'un label pour la version du jeu.

--- fr/cop/menu/PanelMenu.java
+++ fr/cop/menu/PanelMenu.java
@@ -1,11 +1,17 @@
 package fr.cop.menu;
 
+import java.awt.Desktop;
 import java.awt.Polygon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import fr.cop.game.core.Conflict_Of_Pixels_Client;
@@ -13,58 +19,60 @@ import fr.cop.game.core.Conflict_Of_Pixels_Client;
 @SuppressWarnings("serial")
 public class PanelMenu extends JPanel {
 
+	private JLabel versionLabel;
+	private JButton twitterButton;
 	public PanelMenu() {
-		setSize(1280, 720); // On établi la taille de notre paneau.
+		setSize(1280, 720); // On ï¿½tabli la taille de notre paneau.
 		int[] x = { 0, 400, 512, 400, 0 }; // On definit un tableau portant des
 											// coordonnes de points en x.
 		int[][] y = { { 180, 180, 360, 252, 252 }, { 252, 252, 360, 324, 324 }, { 324, 324, 360, 397, 397 },
 				{ 397, 397, 360, 469, 469 }, { 469, 469, 360, 541, 541 } };
 		// Et la correspondance en y, en fonction du bouton
-		setLayout(null); // On supprime le layout manager, afin de placé nos
-							// éléments sur le bon pixel.
+		setLayout(null); // On supprime le layout manager, afin de placï¿½ nos
+							// ï¿½lï¿½ments sur le bon pixel.
 		
-		// On créé un bouton play, avec sa position, son diametre et son arriere
-		// plan. Puis on définis les bords du boutons.
+		// On crï¿½ï¿½ un bouton play, avec sa position, son diametre et son arriere
+		// plan. Puis on dï¿½finis les bords du boutons.
 		PlayButton pb = new PlayButton(512, 360, 100,
 				new ImageIcon(getClass().getResource("/fr/cop/resources/menus/menu_round.png")));
 		pb.setBounds(0, 0, getWidth(), getHeight());
 
-		// On créé un bouton de menu, avec un pilygon comme base, prenant en
-		// parametre nos coordonnées précedentes. On y met un text, et un arrier
-		// plan. Puis on définit les bords du boutons.
+		// On crï¿½ï¿½ un bouton de menu, avec un pilygon comme base, prenant en
+		// parametre nos coordonnï¿½es prï¿½cedentes. On y met un text, et un arrier
+		// plan. Puis on dï¿½finit les bords du boutons.
 		MenusButton v0 = new MenusButton(new Polygon(x, y[0], x.length), "Profil",
 				new ImageIcon(getClass().getResource("/fr/cop/resources/menus/menu_haut_2.png")), pb);
 		v0.setBounds(0, 0, getWidth(), getHeight());
-		// On créé un bouton de menu, avec un pilygon comme base, prenant en
-		// parametre nos coordonnées précedentes. On y met un text, et un arrier
-		// plan. Puis on définit les bords du boutons.
+		// On crï¿½ï¿½ un bouton de menu, avec un pilygon comme base, prenant en
+		// parametre nos coordonnï¿½es prï¿½cedentes. On y met un text, et un arrier
+		// plan. Puis on dï¿½finit les bords du boutons.
 
 		MenusButton v1 = new MenusButton(new Polygon(x, y[1], x.length), "Shop",
 				new ImageIcon(getClass().getResource("/fr/cop/resources/menus/menu_haut_1.png")), pb);
 		v1.setBounds(0, 0, getWidth(), getHeight());
-		// On créé un bouton de menu, avec un pilygon comme base, prenant en
-		// parametre nos coordonnées précedentes. On y met un text, et un arrier
-		// plan. Puis on définit les bords du boutons.
+		// On crï¿½ï¿½ un bouton de menu, avec un pilygon comme base, prenant en
+		// parametre nos coordonnï¿½es prï¿½cedentes. On y met un text, et un arrier
+		// plan. Puis on dï¿½finit les bords du boutons.
 
 		MenusButton v2 = new MenusButton(new Polygon(x, y[2], x.length), "News",
 				new ImageIcon(getClass().getResource("/fr/cop/resources/menus/menu_middle.png")), pb);
 		v2.setBounds(0, 0, getWidth(), getHeight());
-		// On créé un bouton de menu, avec un pilygon comme base, prenant en
-		// parametre nos coordonnées précedentes. On y met un text, et un arrier
-		// plan. Puis on définit les bords du boutons.
+		// On crï¿½ï¿½ un bouton de menu, avec un pilygon comme base, prenant en
+		// parametre nos coordonnï¿½es prï¿½cedentes. On y met un text, et un arrier
+		// plan. Puis on dï¿½finit les bords du boutons.
 
 		MenusButton v3 = new MenusButton(new Polygon(x, y[3], x.length), "Friends",
 				new ImageIcon(getClass().getResource("/fr/cop/resources/menus/menu_bas_1.png")), pb);
 		v3.setBounds(0, 0, getWidth(), getHeight());
-		// On créé un bouton de menu, avec un pilygon comme base, prenant en
-		// parametre nos coordonnées précedentes. On y met un text, et un arrier
-		// plan. Puis on définit les bords du boutons.
+		// On crï¿½ï¿½ un bouton de menu, avec un pilygon comme base, prenant en
+		// parametre nos coordonnï¿½es prï¿½cedentes. On y met un text, et un arrier
+		// plan. Puis on dï¿½finit les bords du boutons.
 
 		MenusButton v4 = new MenusButton(new Polygon(x, y[4], x.length), "Options",
 				new ImageIcon(getClass().getResource("/fr/cop/resources/menus/menu_bas_2.png")), pb);
 		v4.setBounds(0, 0, getWidth(), getHeight());
 
-		// On ajoute une action à notre bouton play.
+		// On ajoute une action ï¿½ notre bouton play.
 		pb.addActionListener(new ActionListener() {
 
 			@Override
@@ -116,17 +124,34 @@ public class PanelMenu extends JPanel {
 			}
 		});
 
-		// On créé un paneau pour nos details de menus (ex : news). Puis on lui
+		// On crï¿½ï¿½ un paneau pour nos details de menus (ex : news). Puis on lui
 		// mets des bords.
 		DetailsPanel detailPanel = new DetailsPanel();
 		detailPanel.setBounds(50, 108, getWidth(), getHeight() - 200);
 
-		// On créé un paneau pour notre barre de profil. Puis on lui
+		// On crï¿½ï¿½ un paneau pour notre barre de profil. Puis on lui
 		// mets des bords.
 		ProfilBarPanel profilBar = new ProfilBarPanel();
 		profilBar.setBounds(0, 0, getWidth(), getHeight());
+		
 
-		// On ajoute tout à notre paneau de menu.
+		versionLabel = new JLabel("Version : alpha-0.0.0.2");
+		versionLabel.setBounds(10, getHeight() - 55, 200, 30);
+
+		twitterButton = new JButton("Twitter");
+		twitterButton.setBounds(getWidth() - 100, 10, 80, 30);
+		twitterButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Desktop.getDesktop().browse(new URI("https://twitter.com/TheGagoi"));
+				} catch (IOException | URISyntaxException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		// On ajoute tout ï¿½ notre paneau de menu.
 		add(v0);
 		add(v1);
 		add(v2);
@@ -135,5 +160,7 @@ public class PanelMenu extends JPanel {
 		add(pb);
 		add(profilBar);
 		add(detailPanel);
+		add(versionLabel);
+		add(twitterButton);
 	}
 }

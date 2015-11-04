7e52e19ea069ea14c87a74ba1e15857a205b4a70
Gagoi <harrygag@gmail.com>
2015-11-04 08:00:22 +0100

    Début de changement pour le sysème de log, afin de les stocker dans un fichier.

--- fr/cop/game/core/helpful/logger/SimpleLog.java
+++ fr/cop/game/core/helpful/logger/SimpleLog.java
@@ -1,12 +1,32 @@
 package fr.cop.game.core.helpful.logger;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import fr.cop.game.core.Game_Frame;
+
 public class SimpleLog {
-	
-	public void logTxt(String tag, String message){
+
+	public File logFile = new File(Game_Frame.gameFolder, "log.txt");
+	private FileOutputStream logFileOut;
+
+	public SimpleLog() {
+		if (!Game_Frame.gameFolder.exists()) Game_Frame.gameFolder.mkdirs();
+		if (!logFile.exists()) try {
+			logFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public void logTxt(String tag, String message) {
 		System.out.println(tag + " : " + message);
 	}
-	
-	public void logErr(String tag, String message){
+
+	public void logErr(String tag, String message) {
 		System.err.println(tag + " : " + message);
 	}
 }

854cca63113bb81beaca92cccb70f1aa11a8b1d5
Gagoi <Gagoi@Gagoi-PC>
2015-12-26 12:51:54 +0100

    Modifications légères afin d'afficher mieux les stats, pas encore terminé.

--- fr/cop/game/graphics/hud/HUD_Statistiques.java
+++ fr/cop/game/graphics/hud/HUD_Statistiques.java
@@ -14,22 +14,31 @@ public class HUD_Statistiques extends HUD_Element {
 
 	public HUD_Statistiques() {
 		super("HUD_Stats", 800, 1080);
-		this.setScale(0.2f);
+		this.setScale(0.5f);
 		this.setCoords(0, Game_Frame.size.height - getScaledHeight());
 
 		Stats s = new Stats();
-		s.setStat(Stats.STAT_ID_Physical_Damage, 500);
-		s.setStat(Stats.STAT_ID_Physical_Resistance, 35);
+		s.setStat(Stats.STAT_ID_Physical_Damage, 0);
+		s.setStat(Stats.STAT_ID_Magic_Damage, 1);
+		s.setStat(Stats.STAT_ID_Physical_Resistance, 2);
+		s.setStat(Stats.STAT_ID_Magic_Resistance, 3);
+		s.setStat(Stats.STAT_ID_Physical_Pen, 4);
+		s.setStat(Stats.STAT_ID_Magic_Pen, 5);
+		s.setStat(Stats.STAT_ID_Cooldown_Reduction, 6);
+		s.setStat(Stats.STAT_ID_Attack_Speed, 7);
+		s.setStat(Stats.STAT_ID_Move_Speed, 8);
 		statsToDisplay.add(s.getStat(Stats.STAT_ID_Physical_Damage) + "");
 		statsToDisplay.add(s.getStat(Stats.STAT_ID_Magic_Damage) + "");
+		
 		statsToDisplay.add(s.getStat(Stats.STAT_ID_Physical_Resistance) + "");
-		statsToDisplay.add("M Res : " + s.getStat(Stats.STAT_ID_Magic_Resistance));
-		statsToDisplay.add("AS : " + s.getStat(Stats.STAT_ID_Attack_Speed));
-		statsToDisplay.add("Range : " + s.getStat(Stats.STAT_ID_Range));
-		statsToDisplay.add("Crit : " + s.getStat(Stats.STAT_ID_Critical_Chance));
-		statsToDisplay.add("CDR : " + s.getStat(Stats.STAT_ID_Cooldown_Reduction));
-		statsToDisplay.add("RPD : " + s.getStat(Stats.STAT_ID_Returned_Physical_Damage));
-		statsToDisplay.add("RMD : " + s.getStat(Stats.STAT_ID_Returned_Magic_Damage));
+		statsToDisplay.add(s.getStat(Stats.STAT_ID_Magic_Resistance) + "");
+		
+		statsToDisplay.add(s.getStat(Stats.STAT_ID_Physical_Pen) + "");
+		statsToDisplay.add(s.getStat(Stats.STAT_ID_Magic_Pen) + "");
+		
+		statsToDisplay.add(s.getStat(Stats.STAT_ID_Cooldown_Reduction) + "");
+		statsToDisplay.add(s.getStat(Stats.STAT_ID_Attack_Speed) + "");
+		statsToDisplay.add(s.getStat(Stats.STAT_ID_Move_Speed) + "");
 	}
 
 	@Override
@@ -41,14 +50,22 @@ public class HUD_Statistiques extends HUD_Element {
 		g.drawImage(Sprites.physical_damage_icon.getImage(), getPosX() + 5, getPosY() + 10, null);
 		g.drawImage(Sprites.magical_damage_icon.getImage(), getPosX() + getScaledWidth() / 2 - 5, getPosY() + 10, null);
 		g.drawImage(Sprites.physical_resistance_icon.getImage(), getPosX() + 5, getPosY() + 40, null);
-		g.drawImage(Sprites.magical_resistance_icon.getImage(), getPosX()  + getScaledWidth() / 2 - 5, getPosY() + 40, null);
+		g.drawImage(Sprites.magical_resistance_icon.getImage(), getPosX() + getScaledWidth() / 2 - 5, getPosY() + 40, null);
 		g.drawImage(Sprites.physical_penetration_icon.getImage(), getPosX() + 5, getPosY() + 80, null);
-		g.drawImage(Sprites.magical_penetration_icon.getImage(), getPosX()  + getScaledWidth() / 2 - 5, getPosY() + 80, null);
+		g.drawImage(Sprites.magical_penetration_icon.getImage(), getPosX() + getScaledWidth() / 2 - 5, getPosY() + 80, null);
 		g.drawImage(Sprites.cooldown_reduction_icon.getImage(), getPosX() + 5, getPosY() + 120, null);
-		g.drawImage(Sprites.move_speed_icon.getImage(), getPosX()  + getScaledWidth() / 2 - 5, getPosY() + 120, null);
-
+		g.drawImage(Sprites.move_speed_icon.getImage(), getPosX() + getScaledWidth() / 2 - 5, getPosY() + 120, null);
+		g.drawImage(Sprites.attack_speed_icon.getImage(), getPosX() + 5, getPosY() + 160, null);
+		
+		
 		g.drawString(statsToDisplay.get(0), getPosX() + 25, getPosY() + 25);
 		g.drawString(statsToDisplay.get(1), getPosX() + getScaledWidth() / 2 + 20, getPosY() + 25);
 		g.drawString(statsToDisplay.get(2), getPosX() + 25, getPosY() + 55);
+		g.drawString(statsToDisplay.get(3), getPosX() + getScaledWidth() / 2 + 20, getPosY() + 55);
+		g.drawString(statsToDisplay.get(4), getPosX() + 25, getPosY() + 85);
+		g.drawString(statsToDisplay.get(5), getPosX() + getScaledWidth() / 2 + 20, getPosY() + 85);
+		g.drawString(statsToDisplay.get(6), getPosX() + 25, getPosY() + 115);
+		g.drawString(statsToDisplay.get(8), getPosX() + getScaledWidth() / 2 + 20, getPosY() + 115);
+		
 	}
 }
